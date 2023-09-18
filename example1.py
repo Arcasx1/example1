d666c40ba6901ae82ad949c32727a482deff28d4
Arcasx1 <145360678+Arcasx1@users.noreply.github.com>
2023-09-18 10:50:21 -0500

    se cambiaron cosas
    .

---
+++ example1.py
@@ -0,0 +1,24 @@
+"""
+    autor: juan esteban molina
+    fecha: 18 de septiempre
+    objetivo: ejemplo de versionamiento con git desde python
+"""
+import random
+random_number = random.randint(1,10)
+print(random_number)
+
+for i in range(0,10):
+    random_number = random.randrange(20,100,5)
+    print(random_number)
+
+print ("--------------------------------------------------")
+
+for i in range(0,10):
+    random_number = random.random()
+    print(random_number)
+
+print("---------------------------------------------------")
+
+for i in range(0,10):
+    random_number = random.uniform(10.5,10.6)
+    print(random_number)
